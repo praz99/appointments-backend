29ae8870b036da84083d5789df6a4500291deac6
Praz <t.prazwal@gmail.com>
2021-02-10 16:33:47 +0100

    fix app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_makeAppointment', domain: 'makeAppointment-json-api'
+  Rails.application.config.session_store :cookie_store, key: '_make-appointment', domain: 'make-appointment-json-api'
 else
-  Rails.application.config.session_store :cookie_store, key: '_makeAppointment'
+  Rails.application.config.session_store :cookie_store, key: '_make-appointment'
 end
