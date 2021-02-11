ba2e32c528075c844db318252cbd77b542682b6c
Praz <t.prazwal@gmail.com>
2021-02-11 14:21:54 +0100

    generate UsersController and add it to routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  # POST /signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.username, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
