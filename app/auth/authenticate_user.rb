819fdd2856f5f2bd0186ab23064e108d0b76456f
Praz <t.prazwal@gmail.com>
2021-02-11 15:53:33 +0100

    user authentication and authorization complete

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -1,4 +1,4 @@
-def AuthenticateUser
+class AuthenticateUser
   def initialize(username, password)
     @username = username
     @password = password
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = MakeAppointment::Application.credentials.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
@@ -8,6 +8,7 @@ class JsonWebToken
 
   def self.decode(token)
     body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
   rescue JWT::DecodeError => e
     raise ExceptionHandler::InvalidToken, e.message
   end
