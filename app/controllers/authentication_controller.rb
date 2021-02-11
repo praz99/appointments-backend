10c41c5d8e2ff5d7202c0853cc17cef37c9bb573
Praz <t.prazwal@gmail.com>
2021-02-11 14:15:08 +0100

    generate AuthenticationController and add it to routes

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,12 @@
+class AuthenticationController < ApplicationController
+  def authenticate
+    auth_token = AuthenticateUser.new(auth_params[:username], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  
+  post 'auth/login', to: 'authentication#authenticate'
 end
